9190e4025ed67063859bf06c16487b08b65620e9
NalinYet <nalin.yetukuri@unh.edu>
2024-09-30 21:35:11 -0400

    Test functions written for the method  how_often_word()

--- test_how_often_word.py
+++ test_how_often_word.py
@@ -4,3 +4,28 @@ File: test_how_often_word.py
 Developer: Nalin
 Collaborator(s): None
 """
+import pytest
+from notes import Notes
+
+
+def test_how_often_word_without_repetion(sentence):
+    """
+    Test a sentence that has words without repetition
+    """
+    sentence = "Test Notes object with"
+    actual = Notes.how_often_word(sentence)
+    expected = {'Test': 1, 'Notes': 1, 'object': 1, 'with': 1}
+    assert actual == expected
+
+
+def test_hoe_often_word_empty_string(sentence):
+    """
+    Test a sentence that is an empty string
+    """
+    sentence = ""
+    actual = Notes.how_often_word(sentence)
+    expected = {}
+    assert actual == expected
+
+
+pytest.main()
