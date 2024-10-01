ed252505d38b7fc926ec4cdf55d3bc7df7ab30bf
NalinYet <nalin.yetukuri@unh.edu>
2024-09-30 21:16:40 -0400

    Created new test modules for the methods how_often_word() and word_histogram() and wrote docstrings

---
+++ test_how_often_word.py
@@ -0,0 +1,6 @@
+"""
+Test Notes instance method how_often_word()
+File: test_how_often_word.py
+Developer: Nalin
+Collaborator(s): None
+"""
---
+++ test_word_histogram.py
@@ -0,0 +1,6 @@
+"""
+Test Notes instance method word_histogram()
+File: test_word_histogram.py
+Developer: Nalin
+Collaborator(s): None
+"""
